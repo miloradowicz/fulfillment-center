4c4c0cb53ba7aa557b647a4d709b48e927e4e7c4
Ashymov Aziret <aziretashym@gmail.com>
2025-05-20 22:45:36 +0600

    [JE2-291] Configured visibility of parts depending on user role.

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,6 +1,6 @@
 import { Options } from 'check-password-strength'
 
-export const featureProtection = import.meta.env.VITE_FEATURE_PROTECTION_DISABLED !== '0'
+export const featureProtection = import.meta.env.VITE_FEATURE_PROTECTION_DISABLED !== '1'
 
 export const emailRegex = /^(\w+[-.]?\w+)@(\w+)([.-]?\w+)?(\.[a-zA-Z]{2,3})$/
 export const phoneNumberRegex = /^(\+?\d{1,3}[-.\s]?)?(\(?\d{1,4}\)?[-.\s]?)?(\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,4})$/
