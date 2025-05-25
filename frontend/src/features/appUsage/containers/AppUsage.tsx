f857b051f7db75e3236a802b67ba49132c600969
Natalya Botsman <botsmannatashaa@gmail.com>
2025-05-25 22:43:46 +0600

    [JE2-339] Fixed tabs styles, corrected the typo

--- frontend/src/features/appUsage/containers/AppUsage.tsx
+++ frontend/src/features/appUsage/containers/AppUsage.tsx
@@ -49,8 +49,8 @@ const AppUsage = () =>  {
       </div>
 
       <Tabs value={value} onValueChange={handleChange} className="w-full">
-        <TabsList className="mb-5 w-full h-auto rounded-3xl">
-          <div className="inline-flex flex-nowrap px-2 space-x-2 sm:space-x-4 overflow-x-auto">
+        <TabsList className="mb-5 sm:w-auto w-full rounded-3xl">
+          <div className="inline-flex flex-nowrap px-2 space-x-2 sm:space-x-4 overflow-x-auto hide-scrollbar">
             <TabsTrigger className={tabTriggerStyles} value="general">Общее</TabsTrigger>
 
             <ProtectedElement allowedRoles={['super-admin', 'admin', 'manager']}>
